a60a427307faa5c9674ae2b3588b70995ccddfaf
gavr-plus <132135308+gavr-plus@users.noreply.github.com>
2023-07-30 14:29:22 +0500

    Update task_2.py

--- Seminar_02/Homework/task_2.py
+++ Seminar_02/Homework/task_2.py
@@ -3,3 +3,27 @@
 # Программа должна возвращать сумму
 # и *произведение дробей. Для проверки своего
 # кода используйте модуль fractions.
+
+from fractions import Fraction
+
+first = input('Введите первую дробь: ').split('/')
+second = input('Введите первую дробь: ').split('/')
+
+num_summ = int(first[0]) * int(second[1]) + int(second[0]) * int(first[1])
+denom_summ = int(first[1]) * int(second[1])
+summ_fract = [num_summ, denom_summ]
+
+multi_fract = [int(first[0]) * int(second[0]), int(first[1]) * int(second[1])]
+
+a1, b1 = summ_fract
+while b1:
+    a1, b1 = b1, a1 % b1
+a2, b2 = multi_fract
+while b2:
+    a2, b2 = b2, a2 % b2
+
+
+print(f'{summ_fract[0]//a1}/{summ_fract[1]//a1}', f'{multi_fract[0]//a2}/{multi_fract[1]//a2}')
+first_f = Fraction(f'{first[0]}/{first[1]}')
+second_f = Fraction(f'{second[0]}/{second[1]}')
+print(first_f + second_f, first_f * second_f)
